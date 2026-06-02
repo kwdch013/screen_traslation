c5f0f29583f154d94bc8ccf7b086f3c436e632be
doikawa <kwdch.013@gmail.com>
2026-06-02 22:33:25 +0900

    chore: gitignoreの修正と、scriptの削除

--- scripts/install_docker_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-sudo apt update
-sudo apt install -y docker.io docker-compose-v2
-sudo usermod -aG docker "$USER"
-
-echo "Dockerを利用するには、WSL/シェルを再起動してください。"
